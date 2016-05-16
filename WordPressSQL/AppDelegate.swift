af233ad3edd3a734ab918f5c3e12b1c1ca65ec42
Sam Davis <sam@sjd.co>
2016-05-16 18:18:24 +1000

    Initial Commit

---
+++ WordPressSQL/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WordPressSQL
+//
+//  Created by Sam Davis on 16/05/2016.
+//  Copyright © 2016 SJDco. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WordPressSQL/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WordPressSQL
+//
+//  Created by Sam Davis on 16/05/2016.
+//  Copyright © 2016 SJDco. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
